6beeadbaf48c6faeb48cf45871412ddebaad84d3
Corvax <vlvete1990@yandex.ru>
2024-08-09 20:55:16 +0300

    feature: add send message notion

--- src/notion/notion.service.ts
+++ src/notion/notion.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { Cron, CronExpression } from '@nestjs/schedule';
+
 import { Client } from '@notionhq/client';
 import { TelegramService } from 'src/telegram/telegram.service';
 
@@ -22,10 +22,11 @@ export class NotionService {
 
     this.notion = new Client({ auth: notionToken });
     this.logger.log('Notion client initialized');
-    this.handleCron();
+
+    this.setMessage('text');
   }
 
-  async getDatabaseEntries() {
+  async setMessage(text: string) {
     const databaseId = this.configService.get<string>('NOTION_PAGE_ID');
     if (!databaseId) {
       this.logger.error('NOTION_PAGE_ID is not defined');
@@ -33,51 +34,25 @@ export class NotionService {
     }
 
     try {
-      const response = await this.notion.databases.query({
-        database_id: this.configService.get<string>('NOTION_PAGE_ID'),
+      await this.notion.pages.create({
+        parent: {
+          database_id: this.configService.get<string>('NOTION_PAGE_ID'),
+        },
+        properties: {
+          title: {
+            title: [
+              {
+                text: {
+                  content: text,
+                },
+              },
+            ],
+          },
+        },
       });
-
-      const entries = response.results.map((page) => {
-        const properties = (page as any).properties;
-        return {
-          id: page.id,
-          Name: properties.Name.title[0]?.plain_text,
-          Date: properties.Date.date?.start,
-        };
-      });
-
-      return entries;
     } catch (error) {
       this.logger.error(`Error fetching database entries: ${error.message}`);
       throw error;
-    }
-  }
-
-  @Cron(CronExpression.EVERY_10_MINUTES, { timeZone: 'Europe/Moscow' })
-  async handleCron() {
-    try {
-      this.logger.log('Fetching database entries from Notion');
-      const records = await this.getDatabaseEntries();
-      const today = new Date().toISOString().split('T')[0]; // Формат YYYY-MM-DD
-
-      for (const record of records) {
-        if (!record.Date || !record.Name) {
-          this.logger.warn('Entry is missing Date or Name field', record);
-          continue;
-        }
-
-        const recordDate = new Date(record.Date).toISOString().split('T')[0];
-
-        if (recordDate === today) {
-          if (!this.sentMessages.has(record.id)) {
-            const chatId = this.configService.get<string>('TELEGRAM_CHAT_ID');
-            await this.telegramService.sendMessage(chatId, record.Name);
-            this.sentMessages.add(record.id);
-          }
-        }
-      }
-    } catch (error) {
-      this.logger.error('Error in scheduled task', error);
     }
   }
 }
--- src/telegram/telegram.service.ts
+++ src/telegram/telegram.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { InjectBot, Start, Update } from 'nestjs-telegraf';
+import { InjectBot, On, Start, Update } from 'nestjs-telegraf';
 import { Context, Telegraf } from 'telegraf';
 
 @Injectable()
@@ -16,6 +16,15 @@ export class TelegramService {
   @Start()
   async startCommand(ctx: Context) {
     await ctx.reply(`Привет, ${ctx.message.from.first_name}!`);
+  }
+
+  @On('text')
+  async sendMessageNotion(ctx: Context) {
+    const message = ctx.text;
+    const chatId = ctx.chat.id;
+    // TODO: передать сообщение в notion
+
+    await this.bot.telegram.sendMessage(chatId, message);
   }
 
   async sendMessage(chatId: string, text: string) {
